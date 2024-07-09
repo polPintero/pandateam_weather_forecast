f888c0c23b3c37f6b7cbde69d0e8f08a14f4a2bb
Dmitriy <johndoe@example.com>
2024-07-09 16:58:20 +0300

    created widget

--- src/components/moleculs/Widget/Widget.vue
+++ src/components/moleculs/Widget/Widget.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="widget">widget</div>
+  <div class="widget">
+    <div class="widget__city">{{ widget.name }}</div>
+    <div class="widget__temp">{{ widget.main.temp.toFixed() }}</div>
+    <div class="widget__description">{{ widget.weather[0].description }}</div>
+    <div class="widget__extra">
+      <div class="widget__humidity">H:{{ widget.main.humidity }}</div>
+      <div class="widget__wind">W:{{ widget.wind.speed }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -19,4 +27,31 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.widget {
+  width: max-content;
+  padding: 16px;
+  color: var(--txt-color-primary);
+  text-align: center;
+
+  &__city {
+    font-size: 2.2rem;
+  }
+
+  &__temp {
+    font-size: 6rem;
+  }
+
+  &__description {
+    font-size: 1.25rem;
+    color: var(--txt-color-secondary);
+  }
+
+  &__extra {
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+    font-size: 1.25rem;
+  }
+}
+</style>
