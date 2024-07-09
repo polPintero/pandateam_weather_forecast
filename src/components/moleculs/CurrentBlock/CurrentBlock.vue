1bf139062fd1286d1f6e54427a3dbba221d2c4d7
Dmitriy <johndoe@example.com>
2024-07-09 21:25:52 +0300

    basic added header

--- src/components/moleculs/CurrentBlock/CurrentBlock.vue
+++ src/components/moleculs/CurrentBlock/CurrentBlock.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="block">
     <div class="block__header">
-      <div class="block__header__item">Hourly Forecast</div>
-      <div class="block__header__item">Weekly Forecast</div>
+      <div class="block__header__item" :class="{ isActive: activeTab === 'today' }">
+        Hourly Forecast
+      </div>
+      <div class="block__header__item" :class="{ isActive: activeTab !== 'today' }">
+        Weekly Forecast
+      </div>
     </div>
+    <div class="block__content"></div>
   </div>
 </template>
 
@@ -15,6 +20,12 @@ export default {
       type: Object,
       default: () => ({})
     }
+  },
+  data() {
+    return {
+      today: this.weeklyForecast[0],
+      activeTab: 'today'
+    };
   }
 };
 </script>
@@ -29,15 +40,26 @@ export default {
   border-bottom-left-radius: 0px;
   overflow: hidden;
   background: linear-gradient(135deg, #2e335a, #1c1b33);
+  display: flex;
+  flex-direction: column;
 
   &__header {
     display: flex;
     justify-content: space-between;
+    padding: 16px;
 
     &__item {
       flex-grow: 1;
       text-align: center;
+      color: var(--txt-color-secondary);
+      cursor: pointer;
     }
+  }
+
+  &__content {
+    flex-grow: 1;
+    padding: 16px;
+    border-top: 1px solid #7582f4;
   }
 }
 </style>
