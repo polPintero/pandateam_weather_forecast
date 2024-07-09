a65d7ca0a6e400515acd39bec93275c1e12493d8
Dmitriy <johndoe@example.com>
2024-07-09 13:31:22 +0300

    basic created btn

--- src/components/moleculs/Menu/AddBtn.vue
+++ src/components/moleculs/Menu/AddBtn.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="add-btn__wrap">addBtn</div>
+  <div class="add-btn__wrap">
+    <div class="add-btn__container">
+      <div class="add-btn"></div>
+    </div>
+  </div>
+
+  <svg class="clip-path-only">
+    <clipPath id="add-btn-path" clipPathUnits="objectBoundingBox">
+      <path
+        d="M0.434,0 H0.597 C0.721,0,0.758,0.241,0.797,0.487 C0.838,0.741,0.88,1,1,1 H0.016 C0.151,1,0.193,0.741,0.234,0.487 C0.273,0.241,0.31,0,0.434,0"
+      ></path>
+    </clipPath>
+  </svg>
 </template>
 
 <script>
@@ -10,10 +22,79 @@ export default {
 
 <style lang="scss" scoped>
 .add-btn {
+  width: 100%;
+  height: 100%;
+  min-width: 100%;
+  border-radius: inherit;
+  position: relative;
+  cursor: pointer;
+  background-image: linear-gradient(#f5f5f9, #dadfe7);
+  filter: drop-shadow(-10px -10px 20px rgba(255, 255, 255, 0.5))
+    drop-shadow(10px 10px 20px rgba(13, 20, 49, 0.5));
+  box-shadow: 1px 1px 0.5px #ffffff;
+
+  &__container {
+    --size-btn: 64px;
+
+    width: var(--size-btn);
+    height: var(--size-btn);
+    min-width: var(--size-btn);
+    border-radius: 50%;
+    padding: 3px;
+    z-index: 1;
+
+    background: #48319d linear-gradient(135deg, #000000 16%, rgba(255, 255, 255, 0.76));
+  }
+
   &__wrap {
+    --border-width: 1px;
     width: 100%;
     height: 100%;
-    background-color: green;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+
+    &:before,
+    &:after {
+      content: '';
+      position: absolute;
+      clip-path: url(#add-btn-path);
+    }
+
+    &:after {
+      top: var(--border-width);
+      left: var(--border-width);
+      width: calc(100% - calc(var(--border-width) * 2));
+      height: calc(100% - calc(var(--border-width) * 2));
+      background: linear-gradient(#262c51, #3e3f74);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: #7582f4;
+    }
+  }
+
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: MAX(20px, 40%);
+    height: 4px;
+    border-radius: 10px;
+    background: var(--color-primary);
+  }
+
+  &:after {
+    transform: translate(-50%, -50%) rotate(90deg);
   }
 }
 </style>
--- src/components/moleculs/Menu/Menu.vue
+++ src/components/moleculs/Menu/Menu.vue
@@ -1,11 +1,19 @@
 <template>
   <nav class="nav">
-    <ul class="menu">
+    <ul class="nav__menu">
       <li>geo</li>
       <li><AddBtn /></li>
       <li>search</li>
     </ul>
   </nav>
+
+  <svg class="clip-path-only">
+    <clipPath id="menu-path" clipPathUnits="objectBoundingBox">
+      <path
+        d="M0,0.011 C0,0.011,0.195,0.193,0.326,0.239 C0.393,0.262,0.432,0.273,0.5,0.273 C0.568,0.273,0.604,0.262,0.672,0.239 C0.802,0.193,1,0.011,1,0.011 V1 H0 V0.011"
+      ></path>
+    </clipPath>
+  </svg>
 </template>
 
 <script>
@@ -33,27 +41,8 @@ export default {
   &:after {
     content: '';
     position: absolute;
-    clip-path: polygon(
-      -0.2% 0%,
-      4.6% 6.1%,
-      10% 11.3%,
-      15.8% 15.6%,
-      22% 19.5%,
-      28% 22.9%,
-      34.6% 26.8%,
-      41.2% 29.4%,
-      48.8% 30.3%,
-      56.2% 29.4%,
-      62.2% 27.3%,
-      68.8% 23.8%,
-      75.6% 19.9%,
-      82.2% 15.2%,
-      88.4% 10.4%,
-      94.4% 5.2%,
-      100% 0.4%,
-      100.2% 100.9%,
-      0% 100.4%
-    );
+    clip-path: url(#menu-path);
+
     border-bottom-left-radius: inherit;
     border-bottom-right-radius: inherit;
   }
@@ -73,15 +62,15 @@ export default {
     height: 100%;
     background: #7582f4;
   }
-}
 
-.menu {
-  position: relative;
-  z-index: 1;
-  width: 100%;
-  height: 100%;
-  list-style-type: none;
-  display: grid;
-  grid-template-columns: 1fr 3fr 1fr;
+  &__menu {
+    position: relative;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    list-style-type: none;
+    display: grid;
+    grid-template-columns: 1fr 3fr 1fr;
+  }
 }
 </style>
