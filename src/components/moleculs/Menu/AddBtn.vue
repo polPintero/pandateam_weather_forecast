b414ffadf72ab9361dc35bdf44d6a4edb6836e6e
Dmitriy <johndoe@example.com>
2024-07-09 10:01:40 +0300

    added shape background

---
+++ src/components/moleculs/Menu/AddBtn.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="add-btn__wrap">addBtn</div>
+</template>
+
+<script>
+export default {
+  name: 'AddBtn'
+};
+</script>
+
+<style lang="scss" scoped>
+.add-btn {
+  &__wrap {
+    width: 100%;
+    height: 100%;
+    background-color: green;
+  }
+}
+</style>
--- src/components/moleculs/Menu/Menu.vue
+++ src/components/moleculs/Menu/Menu.vue
@@ -1,56 +1,87 @@
 <template>
-  <div class="menu">menu</div>
-
-  <svg width="0" height="0" :style="{ transform: `rotate(180deg)` }">
-    <defs>
-      <clipPath id="clip" clipPathUnits="objectBoundingBox">
-        <path
-          d="
-            M  0,1
-			 L 0,0
-			 L 1,0
-			 L 1,1
-			 C .45 .4, .35 .8, 
-             0 1 Z
-             "
-        />
-      </clipPath>
-    </defs>
-  </svg>
+  <nav class="nav">
+    <ul class="menu">
+      <li>geo</li>
+      <li><AddBtn /></li>
+      <li>search</li>
+    </ul>
+  </nav>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue';
+
 export default {
-  name: 'MenuApp'
+  name: 'MenuApp',
+  components: {
+    AddBtn: defineAsyncComponent(() => import('./AddBtn.vue'))
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.menu {
-  height: 100px;
+.nav {
+  --border-width: 1px;
+
+  position: relative;
+  height: MAX(100px, 13%);
   width: 100%;
   margin-top: auto;
-  background: linear-gradient(#3a3a6a, #25244c);
-  clip-path: polygon(
-    -0.2% 0%,
-    4.6% 6.1%,
-    10% 11.3%,
-    15.8% 15.6%,
-    22% 19.5%,
-    28% 22.9%,
-    34.6% 26.8%,
-    41.2% 29.4%,
-    48.8% 30.3%,
-    56.2% 29.4%,
-    62.2% 27.3%,
-    68.8% 23.8%,
-    75.6% 19.9%,
-    82.2% 15.2%,
-    88.4% 10.4%,
-    94.4% 5.2%,
-    100% 0.4%,
-    100.2% 100.9%,
-    0% 100.4%
-  );
+  border-radius: inherit;
+
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    clip-path: polygon(
+      -0.2% 0%,
+      4.6% 6.1%,
+      10% 11.3%,
+      15.8% 15.6%,
+      22% 19.5%,
+      28% 22.9%,
+      34.6% 26.8%,
+      41.2% 29.4%,
+      48.8% 30.3%,
+      56.2% 29.4%,
+      62.2% 27.3%,
+      68.8% 23.8%,
+      75.6% 19.9%,
+      82.2% 15.2%,
+      88.4% 10.4%,
+      94.4% 5.2%,
+      100% 0.4%,
+      100.2% 100.9%,
+      0% 100.4%
+    );
+    border-bottom-left-radius: inherit;
+    border-bottom-right-radius: inherit;
+  }
+
+  &:after {
+    top: var(--border-width);
+    left: var(--border-width);
+    width: calc(100% - calc(var(--border-width) * 2));
+    height: calc(100% - calc(var(--border-width) * 2));
+    background: linear-gradient(#3a3a6a, #25244c);
+  }
+
+  &:before {
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #7582f4;
+  }
+}
+
+.menu {
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  list-style-type: none;
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
 }
 </style>
